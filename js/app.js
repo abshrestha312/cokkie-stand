fba2b85a86cc234836b53585d5968607c5020be1
Abinash Shrestha <75637766+abshrestha312@users.noreply.github.com>
2021-08-03 15:59:05 -0500

    CSS UPDATED

--- js/app.js
+++ js/app.js
@@ -1,105 +1,138 @@
 'use strict';
-funtion Store(name, minCust, maxCust, avgCookieSales){
-    this.name = name;
-    this.minCust = minCust;
-    this.maxCust = maxCust;
-    this.avgCookieSales = avgCookieSales;
-    this.cookiesHour = [];
-    this.cookiesTotal = 0;
-    this.getCookiesHour = function () {
-        for (let i=0; i < this.maxCust.length; i++){
-            let custHour = Math.random() * (this.maxCust - this.minCust + 1) + this.minCust;
-            this.cookiesHour[i] = Math.floor(custHour * this.avgCookieSales);
-        }
-    };
-    this.getCookieTotal = function() {
-        for (let i =0; i < cookiesHour.length; i++ ) {
-            this.cookieTotal = this.cookieTotal + this.cookiesHour[i];
-        }
-    };
+// ------------------------------ Global Variables ----------------------------//
+const tableElem = document.getElementById('sales');
+// for hours of opperation
+const hoursOfOpperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+const restTableElem = document.getElementById('restProfiles');
+// -------------------------------- Constructor ------------------------------//
+function Store(minCust, maxCust, avgCookiePerSale, name) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookiePerSale = avgCookiePerSale;
+  this.salesHourly = [];
+  Store.allStores.push(this)
 }
 
-Store.prototype.renderStoreData = function(trElem) {
-    makeElem('th', trElem, this.name);
-    for (let i=0; i< this.cookiesHour.length; i++) {
-        makeElem('td', trElem, this.cookiesHour[i]);
-    }
-    makeElem('td', trElem, this.cookieTotal);
-;}
+// -------------------------------- constructor related -------------------------//
+Store.allStores = [];
+Store.prototype.randomCustInRange = function() {
+  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+}
+Store.prototype.calculateSalesPerHour = function() {
+  for (let i = 0; i < hoursOfOpperation.length; i++) {
+    const thisHoursSale = Math.ceil(this.randomCustInRange() * this.avgCookiePerSale);
+    this.salesHourly.push(thisHoursSale);
+  }
+}
 
-function addStore(name, minCust, maxCust, avgCookieSales) {
-    const store = new Store(name, minCust, maxCust, avgCookieSales);
-    storeList.push(store);
+// prototype method for render()
+Store.prototype.renderStore = function(bodyElem) {
+  let grandTotal = 0;
+  const rowElement = document.createElement('tr');
+  bodyElem.appendChild(rowElement);
+  const locationTHElem = document.createElement('th');
+  locationTHElem.textContent = this.name;
+  rowElement.appendChild(locationTHElem);
+  for (let i = 0; i < this.salesHourly.length; i++) {
+    const hourlyTotal = this.salesHourly[i]
+    const tdElem = document.createElement('td');
+    tdElem.textContent = hourlyTotal;
+    grandTotal += hourlyTotal;
+    rowElement.appendChild(tdElem);
   }
-  
-  function storeData() {
-    for (let i = 0; i < storeList.length; i++) {
-      storeList[i].getCookiesHour();
-      storeList[i].getCookieTotals();
+  const grandTotalTHElem = document.createElement('th');
+  grandTotalTHElem.textContent = grandTotal;
+  rowElement.appendChild(grandTotalTHElem);
+}
+
+// -------------------------------- global functions ----------------------------//// makes elements and adds them to the dom
+function makeElement(tagName, parent, textContent) {
+  let element = document.createElement(tagName);
+  if (textContent) {
+    element.textContent = textContent;
+  }
+  parent.appendChild(element);
+  return element;
+}
+
+
+// renders header
+function renderHeader() {
+  const headerElem = makeElement('thead', tableElem, null);
+  const rowElem = makeElement('tr', headerElem, null);
+  makeElement('td', rowElem, null);
+  for (let i = 0; i < hoursOfOpperation.length; i++) {
+    makeElement('th', rowElem, hoursOfOpperation[i]);
+  }
+}
+
+function rendersAllStores() {
+  // create the tbody and append it to the table
+  const bodyElem = makeElement('tbody', tableElem, null);
+  for (let i = 0; i < Store.allStores.length; i++) {
+    let currentStore = Store.allStores[i];
+    currentStore.calculateSalesPerHour();
+    currentStore.renderStore(bodyElem);
+  }
+}
+
+// renders footer
+function renderFooter() {
+  const tfootElem = makeElement('tfoot', tableElem, null);
+  const rowElem = makeElement('tr', tfootElem, null);
+  let hourlyTotal = 0;
+  let grandTotal = 0;
+  makeElement('th', rowElem, 'Hourly Totals')
+  for (let i = 0; i < hoursOfOpperation.length; i++){
+    for (let j = 0; j < Store.allStores.length; j++) {
+      let storesSalesAtHour = Store.allStores[j].salesHourly[i];
+      hourlyTotal += storesSalesAtHour;
     }
+    makeElement('th', rowElem, hourlyTotal);
+    grandTotal += hourlyTotal;
+    hourlyTotal = 0;
   }
-  
-  function renderDataTable() {
-    const storesSectionElem = document.getElementById('locations');
-    const tableElem = makeElem('table', storesSectionElem, null);
-    tableHeader(tableElem);
-    tableBody(tableElem);
-    tableFooter(tableElem);
-  }
-  
-  function tableHeader(tableElem) {
-    const thead = makeElem('thead', tableElem, null);
-    const trElem = makeElem('tr', thead, null);
-    makeElem('th', trElem, null);
-    for (let i = 0; i < hours.length; i++) {
-      makeElem('th', trElem, hours[i]);
-    }
-    makeElem('th', trElem, 'Daily Location Total');
-  }
-  
-  function tableBody(tableElem) {
-    const tBodyElem = makeElem('tbody', tableElem, null);
-    for (let i = 0; i < storeList.length; i++) {
-      const trElem = makeElem('tr', tBodyElem, null);
-      storeList[i].renderStoreData(trElem);
-    }
-  }
-  
-  function tableFooter(tableElem) {
-    const tFootElem = makeElem('tfoot', tableElem, null);
-    const trElem = makeElem('tr', tFootElem, null);
-    makeElem('th', trElem, 'Totals');
-    renderTotals(trElem);
-  }
-  
-  function renderTotals(trElem) {
-    let dailyTotal = 0;
-    for (let hourIndex = 0; hourIndex < hours.length; hourIndex++) {
-      let hourlyTotal = 0;
-      for (let storeArrIndex = 0; storeArrIndex < storeList.length; storeArrIndex++) {
-        hourlyTotal = hourlyTotal + storeList[storeArrIndex].cookiesHour[hourIndex];
-      }
-      makeElem('td', trElem, hourlyTotal);
-      dailyTotal = dailyTotal + hourlyTotal;
-    }
-    makeElem('td', trElem, dailyTotal);
-  }
-  
-  function makeElem(tagName, parent, textContent) {
-    let elem = document.createElement(tagName);
-    if (textContent) {
-      elem.textContent = textContent;
-    }
-    parent.appendChild(elem);
-    return elem;
-  }
-  
-  const storeList = [];
-  const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-  addStore('Seattle', 23, 65, 6.3);
-  addStore('Tokyo', 3, 24, 1.2);
-  addStore('Dubai', 11, 38, 3.7);
-  addStore('Paris', 20, 38, 2.3);
-  addStore('Lima', 2, 16, 4.6);
-  storeData();
-  renderDataTable();
+  makeElement('th', rowElem, grandTotal);
+
+}
+
+
+// function uses a snapshot or state of the event as parameter e and creates a new Restaurant with the specifications from the form and renders it to the table.
+function handleSubmit (e){
+  //used this on form to cancel out data going into URL
+  e.preventDefault();
+  console.log(e);
+  let location = e.target.city.value;
+  let minHourlyCust = e.target.minCust.value;
+  parseInt(minHourlyCust);
+  let maxHourlyCust = e.target.maxCust.value;
+  parseInt(maxHourlyCust);
+  let avgCookiePerCust = e.target.aveCookie.value;
+  parseInt(avgCookiePerCust);
+
+  let newRest = new Restaurants(location, minHourlyCust, maxHourlyCust, avgCookiePerCust);
+  newRest.genRandomCust();
+  newRest.calcSalesPerHour();
+
+//clear out table and re-render.
+restTableElem.innerHTML = '';
+
+renderHeader();
+renderAllRest();
+renderFooter();
+
+//this clears the form out after submit
+e.target.reset();
+}
+
+// ------------------------------ call functions ----------------------------//
+const seattle = new Store(23, 65, 6.3, 'Seattle');
+const tokyo = new Store(3, 24, 1.2, 'Tokyo');
+const dubai = new Store(11,38,3.7, 'Dubai');
+const paris = new Store(20,38,2.3,'Paris');
+const lima = new Store(2, 16, 4.6, 'Lima');
+
+renderHeader();
+rendersAllStores();
+renderFooter();
